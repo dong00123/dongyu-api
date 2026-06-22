e10b8883ab29f89372ad61e79b7e3e779fb68ac6
dong00123 <2414695416@qq.com>
2026-06-22 09:32:58 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,9 +5,13 @@ const app = express();
 const port = process.env.PORT || 8080;
 const publicPath = `${process.cwd()}/public`;
 
-// 中间件
 app.use(cors());
+app.use(express.static(publicPath));
 app.use(express.json({ limit: '10mb' }));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${publicPath}/index.html`);
+});
 
 app.post('/api', async (req, res) => {
     const { query, imageBase64 } = req.body;
